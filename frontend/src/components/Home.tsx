ed78f7cc793725aaa6fd549e4ba98dc863c2f0ec
Nicolas Klein <141509949+nicolaskleinaraujo@users.noreply.github.com>
2025-10-15 22:52:34 -0300

    starts the styles on the home component

--- frontend/src/components/Home.tsx
+++ frontend/src/components/Home.tsx
@@ -11,21 +11,21 @@ const Home = () => {
     }
 
     return (
-        <div>
-            <h2>Olá, eu sou</h2>
-            <h1>Nicolas Klein</h1>
-            <h2>Desenvolvedor Full Stack</h2>
+        <div className="flex justify-center flex-col items-center text-center pt-3 text-[#EBEBEB]">
+            <h3 className="text-[#8C8C8C] text-xl">Olá, eu sou</h3  >
+            <h1 className="text-4xl my-3">Nicolas Klein</h1>
+            <h2 className="text-[#7B9EFF]">Desenvolvedor Full Stack</h2>
 
             <p>Juntando tecnologia com proposito para a solução de problemas de forma moderna e eficiente. Especializado em JavaScript e TypeScript.</p>
 
-            <div>
-                <a href="https://github.com/nicolaskleinaraujo/" target="_blank"><LuGithub /></a>
-                <a href="https://www.linkedin.com/in/nicolaskleinaraujo/" target="_blank"><LuLinkedin /></a>
-                <button onClick={() => copyEmail()}><LuMail /></button>
-                <a href="https://wa.me/5544997666224" target="_blank"><FaWhatsapp /></a>
+            <div className="flex m-3 gap-8">
+                <a href="https://github.com/nicolaskleinaraujo/" target="_blank" className="hover:bg-[#262626] p-2 rounded-md"><LuGithub /></a>
+                <a href="https://www.linkedin.com/in/nicolaskleinaraujo/" target="_blank" className="hover:bg-[#262626] p-2 rounded-md"><LuLinkedin /></a>
+                <button onClick={() => copyEmail()} className="hover:bg-[#262626] p-2 rounded-md cursor-pointer"><LuMail /></button>
+                <a href="https://wa.me/5544997666224" target="_blank" className="hover:bg-[#262626] p-2 rounded-md"><FaWhatsapp /></a>
             </div>
 
-            <img src="./my_photo.jpg" alt="My photo" />
+            <img src="./my_photo.jpg" alt="My photo" className="w-57 h-57 border-[#7B9EFF]/40 border-2 rounded-md opacity-88" />
         </div>
     )
 }
