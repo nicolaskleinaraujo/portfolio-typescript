4a46655f6944cfdc50a2608f2c125895e2fd81a4
Nicolas Klein <141509949+nicolaskleinaraujo@users.noreply.github.com>
2025-10-18 19:19:07 -0300

    creates the about page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,11 @@
 import Home from "./components/Home"
+import About from "./components/About"
 
 function App() {
   return (
     <>
       <Home />
+      <About />
     </>
   )
 }
---
+++ frontend/src/components/About.tsx
@@ -0,0 +1,10 @@
+const About = () => {
+    return (
+        <div className="flex flex-col justify-center text-center items-center my-8 mx-3">
+            <h2 className="text-white text-2xl">Sobre Mim</h2>
+            <p className="text-[#8C8C8C]">Sou um desenvolvedor Full Stack apaixado por tecnologia desde pequeno. Desenvolvo aplicações pensando principalmente em sua utilização no dia a dia do usuario, e atualmente tenho um web service gratuito com em que 8 pessoas o utilizam diariamente.</p>
+        </div>
+    )
+}
+
+export default About
