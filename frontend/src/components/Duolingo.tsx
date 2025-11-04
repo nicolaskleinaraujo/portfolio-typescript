5d294c17e241e47468a88e5930d09ba269b61361
Nicolas Klein <141509949+nicolaskleinaraujo@users.noreply.github.com>
2025-11-03 22:26:47 -0300

    reverted the duolingo colors

--- frontend/src/components/Duolingo.tsx
+++ frontend/src/components/Duolingo.tsx
@@ -2,9 +2,9 @@ import { SiDuolingo } from "react-icons/si"
 
 const Duolingo = () => {
     return (
-        <div className="flex items-center text-center border-[#7B9EFF]/40 border rounded-xl shadow-[#7B9EFF]/20 shadow-lg mb-6 mt-2 gap-2 p-2">
-            <SiDuolingo className="h-6 w-6 text-emerald-500 animate-pulse" />
-            <div>
+        <div className="flex items-center text-center border-emerald-500/40 border rounded-xl shadow-emerald-500/20 shadow-lg mb-6 mt-2 gap-3 p-2">
+            <SiDuolingo className="h-6 w-6 text-emerald-500" />
+            <div className="text-center">
                 <p className="text-2xl font-bold text-white">1000</p>
                 <p className="text-xs text-muted-foreground text-[#8C8C8C]">Duolingo Score</p>
             </div>
