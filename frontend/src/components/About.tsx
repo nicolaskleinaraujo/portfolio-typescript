cb8dbd11b95f8e0bfab279cb60b3b452d151c5be
Nicolas Klein <141509949+nicolaskleinaraujo@users.noreply.github.com>
2025-11-24 20:46:27 -0300

    adds the web service link

--- frontend/src/components/About.tsx
+++ frontend/src/components/About.tsx
@@ -2,7 +2,7 @@ const About = () => {
     return (
         <div className="flex flex-col justify-center text-center items-center my-8 mx-3 text-[#EBEBEB]">
             <h2 className="text-2xl mb-2">Sobre Mim</h2>
-            <p className="text-[#8C8C8C]">Sou um desenvolvedor Full Stack apaixado por tecnologia desde pequeno. Desenvolvo aplicações pensando principalmente em sua utilização no dia a dia do usuario, e atualmente tenho um web service gratuito com em que 8 pessoas o utilizam diariamente.</p>
+            <p className="text-[#8C8C8C]">Sou um desenvolvedor Full Stack apaixado por tecnologia desde pequeno. Desenvolvo aplicações pensando principalmente em sua utilização no dia a dia do usuario, e atualmente tenho um <a href="https://node-automail.nkportfolio.tech" target="_blank" className="underline cursor-pointer">web service gratuito</a> com em que 8 pessoas o utilizam diariamente.</p>
         </div>
     )
 }
